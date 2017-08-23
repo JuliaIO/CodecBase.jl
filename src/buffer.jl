1dd3e63ca8747e2b1fc828a8f00285d45cc2ff69
Kenta Sato <bicycle1885@gmail.com>
2017-08-23 11:04:48 +0900

    remove an unused method of Buffer

--- src/buffer.jl
+++ src/buffer.jl
@@ -7,7 +7,6 @@ mutable struct Buffer
 end
 
 Buffer(bufsize) = Buffer(Vector{UInt8}(bufsize), 0)
-capacity(buffer::Buffer) = length(buffer.data)
 Base.empty!(buffer::Buffer) = buffer.size = 0
 Base.getindex(buffer::Buffer, i::Integer) = getindex(buffer.data, i)
 Base.setindex!(buffer::Buffer, v, i::Integer) = setindex!(buffer.data, v, i)
